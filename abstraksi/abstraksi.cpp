bdc8f67d8c3eb8197f3e56d8ded48cb5ef4fd683
sabilafiridas <sabilafirdias@gmail.com>
2024-05-28 11:49:56 +0700

    membuat class baseClass

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -27,3 +27,4 @@ int main()
     return 0;
 }
 
+//abstraksi: menyembunyikan proses
--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -15,18 +15,3 @@ public:
     }
 };
 
-class derivedClass : public baseClass {
-public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived Class";
-    }
-};
-
-int main()
-{
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-
-}
