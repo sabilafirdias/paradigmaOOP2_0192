aeb588214bf8822ca3f450049fb9dec16d85e272
sabilafiridas <sabilafirdias@gmail.com>
2024-05-28 11:58:00 +0700

    membuat class dengan akses public

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -5,5 +5,13 @@ class remoteLampu {
 private:
     string saklarNo[10];
 
+public:
+    void setSaklarNo(int i, string value) {     //set: mengisi nilai saklar
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {                 //get: mendapatkan nilai saklar. tampilkan string dengan cout
+        return saklarNo[i];
+    }
+};
 
 
