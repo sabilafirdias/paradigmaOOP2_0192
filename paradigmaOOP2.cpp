15ffe7809375f4cd67ced0bc0e561f5cd320907a
sabilafiridas <sabilafirdias@gmail.com>
2024-05-28 11:47:11 +0700

    membuat class inti

--- paradigmaOOP2.cpp
+++ paradigmaOOP2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//polymorphi: untuk memberikan kemampuan dan function
+
 class seseorang {
 public:
     virtual void pesan() = 0;
@@ -9,36 +11,4 @@ public:
     // }
 };
 
-class joko : public seseorang {
-public:
-    // deklarasi
-    void pesan() {
-        // implementasi
-        cout << "Pesan dari joko" << endl;
-    }
-};
-
-class lia : public seseorang {
-public:
-    void pesan() {
-        cout << "Pesan dari lia" << endl;
-    }
-};
-
-int main()
-{
-    seseorang* obyek;
-    joko a;
-    lia b;
-
-    obyek = &a;
-    obyek->pesan();
-    obyek = &b;
-    obyek->pesan();
-
-    //akses method pesan() dari class seseorang
-    // a.seseorang::pesan)();
-
-    return 0;
-}
 
