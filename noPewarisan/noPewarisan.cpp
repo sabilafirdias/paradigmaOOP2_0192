ad023b4a9024895ae4503a2c537ec2d01fc9109a
sabilafiridas <sabilafirdias@gmail.com>
2024-05-28 11:54:14 +0700

    membuat class inti

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -8,18 +8,5 @@ public:
     }
 };
 
-class derivedClass : public baseClass {
-public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived Class"
-    }
-};
 
-int main()
-{
-    derivedClass a;
-    a.perkenalan();
-
-    return 0;
-}
 
