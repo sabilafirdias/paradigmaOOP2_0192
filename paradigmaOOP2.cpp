32205658f8390b5b2e71e57edb5eb1905d939dbb
sabilafiridas <sabilafirdias@gmail.com>
2024-05-28 11:47:44 +0700

    membuat class turunan joko

--- paradigmaOOP2.cpp
+++ paradigmaOOP2.cpp
@@ -11,4 +11,12 @@ public:
     // }
 };
 
+class joko : public seseorang {
+public:
+    // deklarasi
+    void pesan() {
+        // implementasi
+        cout << "Pesan dari joko" << endl;
+    }
+};
 
